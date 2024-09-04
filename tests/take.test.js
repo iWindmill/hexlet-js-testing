b46b96a008b430855b75149ba8a6e9b715f5b32e
Alec Windmill <alsermel@gmail.com>
2024-09-04 20:26:39 +0300

    Solution of the exercise from lesson 3

---
+++ tests/take.test.js
@@ -0,0 +1,23 @@
+// Write tests for the function take(items, n)
+// The function returns the first n items from an array
+// By default, n is equal to 1
+// If n is a negative number, an empty array is returned
+
+import { strict as assert } from 'assert';
+import getFunction from '../functions.js';
+
+const take = getFunction();
+
+// BEGIN (write your solution here)
+
+assert.deepEqual(take([1, 2, 3], 1), [1]);
+
+assert.deepEqual(take([1, 2, 3]), [1]);
+
+assert.deepEqual(take([4, 3], 9), [4, 3]);
+
+assert.deepEqual(take([], 2), []);
+
+assert.deepEqual(take([4, 3], -1), []);
+
+// END
